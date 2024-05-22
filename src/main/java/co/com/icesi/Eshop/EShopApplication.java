40797f52c5a8b27f126112b39f06fdb0cc1997b1
danieljaraba <69940328+danieljaraba@users.noreply.github.com>
2024-05-22 11:04:18 -0500

    Update EShopApplication.java

--- src/main/java/co/com/icesi/Eshop/EShopApplication.java
+++ src/main/java/co/com/icesi/Eshop/EShopApplication.java
@@ -3,7 +3,7 @@ package co.com.icesi.Eshop;
 import co.com.icesi.Eshop.model.*;
 import co.com.icesi.Eshop.model.security.Authorities;
 import co.com.icesi.Eshop.repository.*;
-//import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
